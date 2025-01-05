faa5948a2531a6e204926584a47f7ade3000d9df
Robert Saylor <rsaylor@pathful.com>
2025-01-05 04:51:32 -0500

    dockerfile

--- modules/ecs/iam.tf
+++ modules/ecs/iam.tf
@@ -1,18 +1,18 @@
 resource "aws_iam_role" "ec2-role" {
   assume_role_policy = data.aws_iam_policy_document.app-ec2-role.json
+  description          = "Allows EC2 instances to call AWS services on your behalf."
+  max_session_duration = "3600"
+  name                 = "ec2-role"
+  path                 = "/"
+}
 
-  description = "Allows EC2 instances to call AWS services on your behalf."
-  managed_policy_arns = [
+resource "aws_iam_role_policy_attachments_exclusive" "ec2-role" {
+  role_name   = aws_iam_role.ec2-role.name
+  policy_arns = [
     "arn:aws:iam::aws:policy/SecretsManagerReadWrite",
     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess",
     "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
     "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
-    "arn:aws:iam::aws:policy/AmazonSNSFullAccess",
-    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+    "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   ]
-
-  max_session_duration = "3600"
-  name                 = "ec2-role"
-  path                 = "/"
 }
