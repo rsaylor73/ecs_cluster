f0c10220d11cf4278a61a6844b95417bab4d8188
Robert Saylor <rsaylor@pathful.com>
2025-01-07 18:43:01 -0500

    ALB

---
+++ modules/alb/target_group.tf
@@ -0,0 +1,15 @@
+resource "aws_lb_target_group" "target_group" {
+  name        = "target_group"
+  # 32 character max-length
+  port        = "80"
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+  #slow_start  = var.target_group_slow_start
+
+  health_check {
+    path     = "/"
+    protocol = "HTTP"
+    interval = 60
+  }
+}
