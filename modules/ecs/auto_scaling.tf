3efe893bcd87b53956be8e4a192674231a79497a
Robert Saylor <rsaylor@pathful.com>
2025-01-08 10:21:56 -0500

    ASG

--- modules/ecs/auto_scaling.tf
+++ modules/ecs/auto_scaling.tf
@@ -9,12 +9,12 @@ resource "aws_appautoscaling_target" "ecs_service" {
 }
 
 resource "aws_appautoscaling_policy" "scale_out" {
-  for_each           = toset(local.services)
+  #for_each           = toset(local.services)
   name               = "${var.cluster_name}-scale-out"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_service[each.key].resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_service[each.key].scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_service[each.key].service_namespace
+  resource_id        = aws_appautoscaling_target.ecs_service.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_service.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_service.service_namespace
 
   target_tracking_scaling_policy_configuration {
     target_value       = 80.0  # Target CPU utilization (80%)
@@ -31,9 +31,9 @@ resource "aws_appautoscaling_policy" "scale_in" {
   for_each           = toset(local.services)
   name               = "${var.cluster_name}-scale-in"
   policy_type        = "TargetTrackingScaling"
-  resource_id        = aws_appautoscaling_target.ecs_service[each.key].resource_id
-  scalable_dimension = aws_appautoscaling_target.ecs_service[each.key].scalable_dimension
-  service_namespace  = aws_appautoscaling_target.ecs_service[each.key].service_namespace
+  resource_id        = aws_appautoscaling_target.ecs_service.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_service.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_service.service_namespace
 
   target_tracking_scaling_policy_configuration {
     target_value       = 20.0  # Lower target CPU utilization (20%)
