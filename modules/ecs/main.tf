1803fd0099611ed8e11bc89973d48cb953e2aa4f
Robert Saylor <rsaylor@pathful.com>
2025-01-08 08:03:19 -0500

    updated ecs

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -10,7 +10,7 @@ resource "aws_ecs_task_definition" "nginx" {
   family                   = "nginx"
   network_mode             = "awsvpc"
   cpu                      = "256"
-  memory                   = "256"
+  memory                   = "512"
   requires_compatibilities = ["FARGATE"]
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
